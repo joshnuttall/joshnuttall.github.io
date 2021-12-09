7cb4cc9c46550efab567a76afb08d8bdc440ef0f
joshnuttall <nut19005@byui.edu>
2021-12-08 23:56:15 -0700

    p8

--- chamber-of-commerce/java.js
+++ chamber-of-commerce/java.js
@@ -1,4 +1,40 @@
+
 const url = 'https://joshnuttall.github.io/chamber-of-commerce/directory.json';
+const apiKeey = "7f09a0a170c211455e98942cb47fb75b";
+const lat = 39.71;
+const lon = -104.81;
+const webAdd = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=${apiKeey}&units=imperial`;
+
+fetch(webAdd)
+    .then(function(response){
+        if(response.ok){
+            return response.json();
+        }
+    throw new Error('Network Response was not ok');
+    
+    })
+
+    .then(function(jsonObject){
+        let div = document.createElement('div');
+        let h1 = document.createElement('h1');
+        let currentTemp = document.createElement('p');
+        let description = document.createElement('p');
+        let humidity = document.createElement('p');
+        div.setAttribute('class','current-weather')
+        h1.textContent = "Current Weather";
+        currentTemp.textContent = "Current Temperature: " + Math.round(jsonObject.current.temp) + "°F";
+        description.textContent = "Current Condition: " + jsonObject.current.weather[0].description
+        humidity.textContent = "Humidity: " + Math.round(jsonObject.current.humidity,1) + "%";
+        console.log(jsonObject);
+        div.appendChild(h1);
+        div.appendChild(currentTemp);
+        div.appendChild(description);
+        div.appendChild(humidity);
+        document.querySelector('#main-home').appendChild(div);
+          
+    })
+
+
 
 fetch(url)
     .then(function(response){
@@ -12,15 +48,18 @@ fetch(url)
         const business = jsonObject['business'];
         for(i=0;i<business.length;i++){
         let card = document.createElement('div');
-        let name = document.createElement('p');
-        let address = document.createElement('p');
-        let contact = document.createElement('p');
+        let name = document.createElement('h1');
+        let address = document.createElement('h1');
+        let contact = document.createElement('h1');
         let image = document.createElement('img');
         image.setAttribute('src',jsonObject.business[i].logo);
+        card.setAttribute('class','pic-grid')
+        card.setAttribute('id','pic-' + i)
         name.textContent = jsonObject.business[i].name;
         address.textContent = jsonObject.business[i].address;
         contact.textContent = jsonObject.business[i].contact;
     card.setAttribute('class','cardz')
+    image.setAttribute('alt',name.textContent)
     card.appendChild(name);
     card.appendChild(address);
     card.appendChild(contact);
@@ -28,9 +67,23 @@ fetch(url)
     document.querySelector('main#card-box').appendChild(card);
     
 }
-console.log(jsonObject);
+
 
     })
+
+function grid(){ 
+    document.querySelector('#card-box').setAttribute('class','single')
+        
+}
+
+
+
+function single(){ 
+    for(i=0;i<8;i++){
+    document.querySelector('#card-box').removeAttribute('class');
+    document.querySelector('#card-box').setAttribute('class','grido');
+        
+}}
 
 function currYear()
 {
